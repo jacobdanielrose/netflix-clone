24de36100bedb1254172264555a1cb204958a990
Jacob Rose <jacobdanielrose@protonmail.com>
2023-03-14 20:31:33 +0100

    added the backend next auth logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-     <h1 className={"text-2xl text-red-500"}>Netflix Clone</h1>
+      <h1 className={"text-2xl text-red-500"}>Netflix Clone</h1>
     </>
   )
 }
