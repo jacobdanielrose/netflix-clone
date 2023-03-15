e2a0675ca2162bb74b5c5d07c487985226308ab2
Jacob Rose <jacobdanielrose@protonmail.com>
2023-03-15 18:52:22 +0100

    added session api to get current user

---
+++ hooks/useCurrentUser.ts
@@ -0,0 +1,13 @@
+import fetcher from '@/lib/fetcher'
+import useSWR from 'swr'
+
+export default function useCurrentUser() {
+    const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher)
+
+    return {
+        data,
+        error,
+        isLoading,
+        mutate
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
 import { Inter } from 'next/font/google'
+import { getSession, signOut } from 'next-auth/react'
+import { NextPageContext } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
   return (
     <>
-      <h1 className={"text-2xl text-red-500"}>Netflix Clone</h1>
+      <h1 className={"text-4xl text-red-500"}>Netflix Clone</h1>
+      <button className='h-10 w-40 bg-white'
+        onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   )
 }
