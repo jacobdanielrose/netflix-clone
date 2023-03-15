a8ec4e99cd837b540002080469f007a69bda6b57
Jacob Rose <jacobdanielrose@protonmail.com>
2023-03-15 21:24:43 +0100

    navbar. next is mobile menu

---
+++ components/MobileMenu.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function MobileMenu({ }: Props) {
+    return (
+        <div>MobileMenu</div>
+    )
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,35 @@
+import NavbarItem from "@/components/NavbarItem";
+import MobileMenu from "@/components/MobileMenu";
+
+import { BsChevronDown } from 'react-icons/bs'
+
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <nav className="w-full fixed z-40">
+            <div
+                className="px-4 md:px-10 py-6 flex flex-row
+                items-center 
+                transition duration-500
+                bg-zinc-900
+                bg-opacity-90
+                ">
+                <img className="h-4 lg:h-7" src="/images/logo.png" alt="netflix-logo" />
+                <div className="flex-row ml-8 gap-7 hidden lg:flex">
+                    <NavbarItem label="Home" />
+                    <NavbarItem label="TV Shows" />
+                    <NavbarItem label="Movies" />
+                    <NavbarItem label="New & Popular" />
+                    <NavbarItem label="My Favorites" />
+                    <NavbarItem label="Browse by languages" />
+                </div>
+                <div
+                    className="lg:hidden flex flex-row items-center gap-2
+                    ml-8 cursor-pointer relative">
+                    <p className="text-white text-sm">Browse</p>
+                    <BsChevronDown className="text-white transition" />
+                    <MobileMenu />
+                </div>
+            </div>
+        </nav>
     )
 }
